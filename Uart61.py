8c4e9114a4e0f729880f1e0cd1987b33adc26690
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-01 16:13:44 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -47,7 +47,7 @@ def open_serial_connection_and_print_output():
     while True:
       ser.read_until(binascii.hexlify(b'0x55'))
       reading = ser.read(1)
-      converted = binascii.hexlify(reading)
+      converted = bytes.fromhex(reading)
     # Print the output buffer
 
       print(converted)
