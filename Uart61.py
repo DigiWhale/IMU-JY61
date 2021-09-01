18c15638d02df76d0f01b630193010a1ffae7865
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-01 11:49:00 -0400

    added serial port

--- Uart61.py
+++ Uart61.py
@@ -6,7 +6,7 @@ def open_serial_connection_and_print_output():
     :return: None
     """
     # Open the serial connection
-    ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=1)
+    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1)
     # Flush the input buffer
     ser.flushInput()
     # Read the output buffer
