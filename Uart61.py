84e8b7e26ff87f21a695f6e451aed9ac99beef81
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-01 18:15:27 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -50,7 +50,7 @@ def open_serial_connection_and_print_output():
       reading = ser.read(1)
       # converted = reading.hex()
     # Print the output buffer
-      print(str(reading).hex())
+      print(int(str(reading), 16))
       # print(int(converted, 16))
     # Close the connection
     ser.close()
