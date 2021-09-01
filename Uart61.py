929fbec2f4603666a3739efa0c258c3006964cb2
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-01 19:58:28 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -56,7 +56,7 @@ def open_serial_connection_and_print_output():
         data = b''.join(data)
         for i in range(0, len(data), 1):
           hex_value = binascii.hexlify(data[i:i+1])
-          string = str(hex_value, 'utf-8')
+          string = int(hex_value, '16')
           print(string)
         print('########################')
         # print('data', data.hex())
