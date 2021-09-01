dde38c002deaac50b5b887f81b8741ce0abe9fed
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-01 18:01:30 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -40,13 +40,13 @@ def open_serial_connection_and_print_output():
     :return: None
     """
     # Open the serial connection
-    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=19200, bytesize=8, parity='N', stopbits=1, timeout=10)
+    ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=10)
     # Flush the input buffer
     ser.flushInput()
     # Read the output buffer
     while True:
       # ser.read_until(binascii.hexlify(b'0x55'))
-      reading = ser.readline()
+      reading = ser.read(1)
       # converted = reading.hex()
     # Print the output buffer
       print(reading)
