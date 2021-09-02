9267ad4f9b431b6ac8467091a099974ce1e9fe89
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-02 13:10:16 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -93,9 +93,10 @@ def open_serial_connection_and_print_output():
           signed = combined
           # low_shifted_bit_string = "{:08b}".format(low_shifted)
           if sensor == "51":
-            print('Accelerometer:low', low_shifted, low_byte, low_string)
-            print('Accelerometer:high', high_shifted_bit_string, high_byte, high_string)
-            print('combined', "{:016b}".format(combined), (signed/32768)*16*9.8)
+            print(signed)
+            # print('Accelerometer:low', low_shifted, low_byte, low_string)
+            # print('Accelerometer:high', high_shifted_bit_string, high_byte, high_string)
+            # print('combined', "{:016b}".format(combined), (signed/32768)*16*9.8)
           # elif sensor == "52":
           #   print('Gyroscope:', string)
           # elif sensor == "53":
