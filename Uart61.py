02878a12690401e6db46e8f67553bf93d89a0f0e
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-02 17:27:23 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -37,7 +37,8 @@ def open_serial_connection_and_print_output():
         # print(data)
         for i in range(0, len(data), 2):
           # pass
-          binaryToDecimal(binascii.hexlify(data[i:i+2]))
+          x=binascii.hexlify(data[i:i+2])
+          binaryToDecimal(int(x.decode('UTF-8'), 16))
           # print(binascii.hexlify(data[i:i+2]))
           # # read 1st bit to identify sensor message
           # sensor = binascii.hexlify(data[0:1]).decode('UTF-8')
