b7e720c6c8cc7c8634dfb9e7066e7b6ed85d42ac
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-02 22:04:15 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -38,17 +38,18 @@ def open_serial_connection_and_print_output():
         # print(data)
         header = binascii.hexlify(data[0:1]).decode('UTF-8')
         sensor = binascii.hexlify(data[1:2]).decode('UTF-8')
-        reg_1 = data[2:4]
-        reg_2 = data[4:6]
-        reg_3 = data[6:8]
-        reg_4 = data[8:10]
-        checksum = binascii.hexlify(data[10:11]).decode('UTF-8')
-        print(header, sensor, reg_1, reg_2, reg_3, reg_4, checksum)
-        dec_reg_1 = int.from_bytes(reg_1, byteorder=sys.byteorder, signed=True)
-        dec_reg_2 = int.from_bytes(reg_2, byteorder=sys.byteorder, signed=True)
-        dec_reg_3 = int.from_bytes(reg_3, byteorder=sys.byteorder, signed=True)
-        dec_reg_4 = int.from_bytes(reg_4, byteorder=sys.byteorder, signed=True)
-        print(header, sensor, dec_reg_1, dec_reg_2, dec_reg_3, dec_reg_4, checksum)
+        if sensor == 51:
+          reg_1 = data[2:4]
+          reg_2 = data[4:6]
+          reg_3 = data[6:8]
+          reg_4 = data[8:10]
+          checksum = binascii.hexlify(data[10:11]).decode('UTF-8')
+          print(header, sensor, reg_1, reg_2, reg_3, reg_4, checksum)
+          dec_reg_1 = int.from_bytes(reg_1, byteorder=sys.byteorder, signed=True)
+          dec_reg_2 = int.from_bytes(reg_2, byteorder=sys.byteorder, signed=True)
+          dec_reg_3 = int.from_bytes(reg_3, byteorder=sys.byteorder, signed=True)
+          dec_reg_4 = int.from_bytes(reg_4, byteorder=sys.byteorder, signed=True)
+          print(header, sensor, dec_reg_1, dec_reg_2, dec_reg_3, dec_reg_4, checksum)
         for i in range(2, len(data), 2):
           pass
           # print(data[i:i+2])
