947b74e413f9960e8fa2132b18a1f54f595b88a6
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-03 19:15:13 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -70,7 +70,7 @@ def open_serial_connection_and_print_output(export_array):
             export_array.insert(0, accel)
             export_array.insert(1, velocity)
             export_array.insert(2, angle)
-            export_array = export_array[:3]
+            export_array = export_array[:-3]
             # for key, value in angle.items():
             #   print(key, ' : ', value)
             # for key, value in velocity.items():
