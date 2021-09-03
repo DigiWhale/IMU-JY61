02d69595b7c9dfdd6254d653eb8dc3638216d1ab
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-03 18:31:19 -0400

    1

--- thread_practice.py
+++ thread_practice.py
@@ -25,16 +25,18 @@ def modify_variable_2(var):
 your_var = [0, 1, 2]
 my_var = [1, 2, 3]
 t = Thread(target=open_serial_connection_and_print_output)
-t.start()
 b = Thread(target=modify_variable_2, args=(your_var, ))
 b.start()
 while True:
+    t.start()
+
     try:
         # print(my_var, your_var)
         sleep(.01)
     except KeyboardInterrupt:
         event.set()
         break
+    t.join()
 t.join()
 b.join()
 print('done')
