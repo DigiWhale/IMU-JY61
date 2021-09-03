7bfb9d54992824d1845369456827acd76977d293
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-03 00:13:00 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -48,8 +48,8 @@ def open_serial_connection_and_print_output():
           dec_reg_2 = int.from_bytes(reg_2, byteorder=sys.byteorder, signed=True)/32768*16
           dec_reg_3 = int.from_bytes(reg_3, byteorder=sys.byteorder, signed=True)/32768*16
           dec_reg_4 = int.from_bytes(reg_4, byteorder=sys.byteorder, signed=True)/340+36.53
-          dec_checksum = int.from_bytes(checksum, byteorder=sys.byteorder)
-          print(header, sensor, dec_reg_1, dec_reg_2, dec_reg_3, dec_reg_4, dec_checksum, sum_checksum + 85)
+          dec_checksum = int.from_bytes(checksum, byteorder=sys.byteorder ,signed=True)
+          print(header, sensor, dec_reg_1, dec_reg_2, dec_reg_3, dec_reg_4, dec_checksum, sum_checksum)
         for i in range(2, len(data), 2):
           pass
           # print(data[i:i+2])
