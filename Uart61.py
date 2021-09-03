37a0860451809b97bab04f8ac446fa15bfabf035
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-02 22:50:50 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -35,15 +35,15 @@ def open_serial_connection_and_print_output():
         reg_3 = data[6:8]
         reg_4 = data[8:10]
         checksum = data[10:11]
-        checked = header_raw + sensor_raw + reg_1 + reg_2 + reg_3 + reg_4
+        sum_of_data = reg_1 + reg_2 + reg_3 + reg_4
         if sensor == b'51':
           # print(header, sensor, reg_1, reg_2, reg_3, reg_4, checksum)
           dec_reg_1 = int.from_bytes(reg_1, byteorder=sys.byteorder, signed=True)/32768*16
           dec_reg_2 = int.from_bytes(reg_2, byteorder=sys.byteorder, signed=True)/32768*16
           dec_reg_3 = int.from_bytes(reg_3, byteorder=sys.byteorder, signed=True)/32768*16
           dec_reg_4 = int.from_bytes(reg_4, byteorder=sys.byteorder, signed=True)/340+36.53
-          dec_checksum = int.from_bytes(checked, byteorder=sys.byteorder, signed=False)
-          print(header, header_raw, sensor, sensor_raw, dec_reg_1, dec_reg_2, dec_reg_3, dec_reg_4, checksum, dec_checksum, checked)
+          # dec_checksum = int.from_bytes(checked, byteorder=sys.byteorder, signed=False)
+          print(header, header_raw, sensor, sensor_raw, dec_reg_1, dec_reg_2, dec_reg_3, dec_reg_4, checksum, sum_of_data)
         for i in range(2, len(data), 2):
           pass
           # print(data[i:i+2])
