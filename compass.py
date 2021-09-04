8f08655465c104514c023efb48081941274a4ab8
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-03 20:01:46 -0400

    1

--- compass.py
+++ compass.py
@@ -6,6 +6,6 @@ def get_compass_value(compass):
     Return the compass value of the current heading.
     """
     while True:
-      compass[:] = {'heading': random.randint(0, 360)}.items()
+      compass[:] = {'heading': 0}.items()
       time.sleep(0.001)
       
