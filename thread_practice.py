1c56ebf117c6ea2096bfefb765a0d37626e210b2
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-03 20:24:12 -0400

    1

--- thread_practice.py
+++ thread_practice.py
@@ -16,7 +16,7 @@ while True:
     try:
         # print(compass, velocity, angle, accel)
         try:
-          heading = compass[0][1] + angle[3][1] + 180
+          heading = compass[0][1] + angle[2][1] + 180
           print(heading)
         except:
           print('passed')
