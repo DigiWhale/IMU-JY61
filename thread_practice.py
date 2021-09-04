e02d1fe23ab4348516c7d0c3112ee33b65eaaf5a
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-03 20:05:43 -0400

    1

--- thread_practice.py
+++ thread_practice.py
@@ -4,26 +4,6 @@ from Uart61 import open_serial_connection_and_print_output
 from compass import get_compass_value
 event = Event()
 
-def modify_variable(var):
-    while True:
-        for i in range(len(var)):
-            var[i] += 1
-        if event.is_set():
-            break
-        sleep(.001)
-    print('Stop printing')
-    
-def modify_variable_2(var):
-    while True:
-        for i in range(len(var)):
-            var[i] += 2
-        if event.is_set():
-            break
-        sleep(1)
-    print('Stop printing')
-
-your_var = [0, 1, 2]
-my_var = [1, 2, 3]
 velocity = []
 accel = []
 angle = []
@@ -34,7 +14,9 @@ t.start()
 b.start()
 while True:
     try:
-        print(compass, velocity, angle, accel)
+        # print(compass, velocity, angle, accel)
+        heading = compass[0][1]
+        print(heading)
     except KeyboardInterrupt:
         event.set()
         break
