c83590572d66d4f2bafe2a05995e1a3490fea9c4
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-04 18:52:52 -0400

    1

--- thread_practice.py
+++ thread_practice.py
@@ -26,12 +26,10 @@ while True:
             init_compass = True
           heading = compass[0][1] - ((angle[2][1]+180) - compass[0][1])
           imu_heading = angle[2][1] - offset
-          print('heading', heading)
-          print('compass', compass[0][1])
-          print('imu_heading', imu_heading)
+          print('heading', heading, 'compass', round(compass[0][1]), 'imu_heading', round(imu_heading), 'imu_compensated', round((angle[2][1] + 360 if angle[2][1] < 0 else angle[2][1])), end='\r')
           # print(round(heading), round(compass[0][1]), round(angle[2][1] - compass[0][1]), round((angle[2][1]+180) - compass[0][1]), round(angle[2][1] - compass[0][1]), round((angle[2][1]+180) - compass[0][1]-offset))
-          print('difference', round((angle[2][1]+180) - compass[0][1]-offset) - round(compass[0][1]))
-          print('#################################')
+          # print('difference', round((angle[2][1]+360 if angle[2][1] < 0 else angle[2][1]) - compass[0][1]-offset) - round(compass[0][1]))
+          # print('#################################')
           sleep(0.01)
           if event.is_set():
             break
