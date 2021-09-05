d74b5c0a553880958be80b71223895b656144bf0
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-04 22:01:26 -0400

    1

--- Uart61.py
+++ Uart61.py
@@ -17,8 +17,8 @@ def open_serial_connection_and_print_output(angle_list, velocity_list, accel_lis
       velocity_ready = False
       angle_ready = False
       while True:
-          time.sleep(0.01)
           ser.flushInput()
+          time.sleep(0.01)
           # initialize data buffer to store bytes
           data = []
           # start reading bytes
